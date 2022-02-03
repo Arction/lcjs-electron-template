98d14ae704e545a7e58a869dad6f3e9c2b3be702
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2022-02-03 12:37:23 +0200

    Update to LCJS 3.4.0. Format files

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 1000,
     webPreferences: {
-      preload: path.join(__dirname, 'script.js')
-    }
-  })
+      preload: path.join(__dirname, "script.js"),
+    },
+  });
 
-  win.loadFile('index.html' )
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-function Line_series(json){
-    const JsonData = eval(json);
-      window.data  = JsonData;
-    Lineseries(data);
+function Line_series(json) {
+  const JsonData = eval(json);
+  window.data = JsonData;
+  Lineseries(data);
 }
-function LineSeries(){
-    $.getJSON("data.json", function(json) {
-       window.scope = json;
-    });
-    Line_series(scope);
+function LineSeries() {
+  $.getJSON("data.json", function (json) {
+    window.scope = json;
+  });
+  Line_series(scope);
 }
