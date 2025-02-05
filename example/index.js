0e58c6ced6ff9f03630a85d001ec044756b3e5ba
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2025-02-05 14:41:28 +0200

    LCJS v7.0

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 /// <reference path="../node_modules/@lightningchart/lcjs/dist/lcjs.iife.d.ts" />
 
-const { lightningChart, AxisScrollStrategies, AxisTickStrategies } = lcjs;
+const { lightningChart, AxisScrollStrategies, AxisTickStrategies, emptyFill } =
+  lcjs;
 
 const lc = lcjs.lightningChart({
   // Get your license key here: https://lightningchart.com/js-charts/#license-key
@@ -11,25 +12,25 @@ const container = document.getElementById("chart");
 const chart = lc
   .ChartXY({ container, defaultAxisX: { type: "linear-highPrecision" } })
   .setTitle("LightningChart JS in Electron");
-const axisX = chart
-  .getDefaultAxisX()
+const axisX = chart.axisX
   .setScrollStrategy(AxisScrollStrategies.progressive)
   .setInterval({ start: -15_000, end: 0, stopAxisAfter: false })
   .setTickStrategy(AxisTickStrategies.DateTime)
   .setAnimationScroll(false);
 const axisY = chart.getDefaultAxisY();
 const lineSeries = chart
-  .addLineSeries({
-    dataPattern: { pattern: "ProgressiveX" },
+  .addPointLineAreaSeries({
+    dataPattern: "ProgressiveX",
   })
-  .setDataCleaning({ minDataPointCount: 1 });
+  .setAreaFillStyle(emptyFill)
+  .setMaxSampleCount(100_000);
 
 // Connect to WebSocket server hosted on Electron main process (main.js)
 // This is used as a powerful medium to stream data into the LightningChart, wherever it may need to come from.
 const ws = new WebSocket(`ws://localhost:${WS_PORT}`);
 ws.onmessage = (event) => {
   const { x, y } = JSON.parse(event.data);
-  lineSeries.add([{ x, y }]);
+  lineSeries.appendJSON([{ x, y }]);
   // NOTE: This example is a minimal reproduction of transferring and visualizing real-time data in Electron.
   // For more details, please refer to general materials (not tied to Electron).
   // https://lightningchart.com/js-charts/docs/basic-topics/real-time-data/
